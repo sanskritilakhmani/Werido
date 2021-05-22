aca0d74ff407030318c18b9f63b3c46d2b04fe62
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-05-22 14:00:02 +0530

    Add files via upload

---
+++ Package_Password.java
@@ -0,0 +1,27 @@
+public class Package_Password {
+    // que-> create Caesar Cipher Password as
+    // 1) change all uppercase to lowercase
+    // 2) char = char + 2
+package myPractice;
+import java.util.Scanner;
+import java.lang.*;
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Please Enter Your Password");
+            String inputPassword = scn.nextLine();
+            String password = "";
+            for (char i : inputPassword.toCharArray()){
+                if (Character.isUpperCase(i)){
+                    password += (char) (Character.toLowerCase(i) + 2);
+                }
+                else {
+                    password += (char) (i + 2);
+                }
+            }
+            System.out.println("Its Caesar Cipher Code is \n"+password);
+        }
+    }
+
+
+}
